8dba73ce2af1c70a2a38f718f546fe2e1076669b
Khaled AbdElRahim <khaled710017@gmail.com>
2022-02-05 17:39:45 +0200

    Add comments for CollectionService

--- app/Services/CollectionService.php
+++ app/Services/CollectionService.php
@@ -9,7 +9,15 @@ use Illuminate\Pagination\LengthAwarePaginator;
 
 class CollectionService
 {
-    public static function paginate(Collection $results, $pageSize)
+    /**
+     * paginate a given collection
+     *
+     * @param Collection $results
+     * @param $pageSize
+     * @return LengthAwarePaginator
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public static function paginate(Collection $results, $pageSize): LengthAwarePaginator
     {
         $page = Paginator::resolveCurrentPage('page');
 
@@ -25,14 +33,15 @@ class CollectionService
     /**
      * Create a new length-aware paginator instance.
      *
-     * @param  \Illuminate\Support\Collection  $items
-     * @param  int  $total
-     * @param  int  $perPage
-     * @param  int  $currentPage
-     * @param  array  $options
+     * @param \Illuminate\Support\Collection $items
+     * @param int $total
+     * @param int $perPage
+     * @param int $currentPage
+     * @param array $options
      * @return \Illuminate\Pagination\LengthAwarePaginator
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
-    protected static function paginator($items, $total, $perPage, $currentPage, $options)
+    protected static function paginator($items, $total, $perPage, $currentPage, $options): LengthAwarePaginator
     {
         return Container::getInstance()->makeWith(LengthAwarePaginator::class, compact(
             'items', 'total', 'perPage', 'currentPage', 'options'
