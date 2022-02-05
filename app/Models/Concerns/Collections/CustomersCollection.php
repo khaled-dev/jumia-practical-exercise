3fa6bf78f1ac767c0136dd7ad62af32b628c8bda
Khaled AbdElRahim <khaled710017@gmail.com>
2022-02-05 17:11:35 +0200

    Add customer model/collection

---
+++ app/Models/Concerns/Collections/CustomersCollection.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Concerns\Collections;
+
+use Illuminate\Database\Eloquent\Collection;
+
+
+class CustomersCollection extends Collection
+{
+
+}
---
+++ app/Models/Customer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Concerns\Collections\CustomersCollection;
+use App\Services\Builders\CustomerDataBuilder;
+use App\Services\CollectionService;
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'customer';
+
+    /**
+     * Create a new Eloquent Collection instance.
+     *
+     * @param array $models
+     * @return CustomersCollection
+     */
+    public function newCollection(array $models = []): CustomersCollection
+    {
+        return new CustomersCollection($models);
+    }
+}
