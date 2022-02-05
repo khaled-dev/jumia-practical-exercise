e3663784775913e315d0180bba1908d7e61844bc
Khaled AbdElRahim <khaled710017@gmail.com>
2022-02-05 22:14:06 +0200

    Configure appJs for vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import VueAxios from "vue-axios";
+import axios from "axios";
+import App from './views/app.vue';
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(VueAxios, axios);
+Vue.use(BootstrapVue)
+
+
+new Vue(Vue.util.extend(App)).$mount('#app');
---
+++ resources/js/config.js
@@ -0,0 +1,7 @@
+
+export const API_BASE_URL = 'http://localhost/api';
+
+export const API_COUNTRIES_URL = API_BASE_URL + '/countries';
+
+export const API_USERS_URL = API_BASE_URL + '/customers';
+
